8fe4299663ef3bdca9f9b33a21d3900951903d21
Ankit_Raj <ankitrajlogin@gmail.com>
2024-11-13 01:12:18 +0530

    Update 13_3_void_function_pointer.cpp

--- Pointer in c and c++/13_3_void_function_pointer.cpp
+++ Pointer in c and c++/13_3_void_function_pointer.cpp
@@ -1,15 +1,22 @@
 
 
 #include<bits/stdc++.h>
-using namespace std ; 
+using namespace std ;
 
-void printHello(char* name){
-    cout << name << endl ; 
+void printHello(char* name) {
+	cout << name << endl ;
 }
 
-int main(){
-    void (*ptr)(char*) ; 
+// The syntax void (*ptr)(char*) is used to declare a function pointer. Here:
+// 1. void is the return type.
+// 2. (*ptr) declares ptr as a pointer to a function.
+// 3. (char*) specifies that the function pointed to by ptr takes a char* as its parameter.
 
-    ptr = printHello ; 
-    ptr("Tom") ; 
+int main() {
+	// This line declares a pointer ptr that can point to a function taking a char* as an argument and returning void.
+	void (*ptr)(char*) ;
+    
+    // This line calls the function printHello through the pointer ptr, passing "Tom" as the argument.
+	ptr = printHello ;
+	ptr("Tom") ;
 }
